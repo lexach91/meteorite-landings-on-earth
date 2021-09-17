f1e713cf11648336632d8828469171fba52aa3aa
lexach91 <konovalov1991@mail.ru>
2021-09-17 13:39:04 +0800

    Experiment with plotting data on different charts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+const nameTypeChart = dc.rowChart("#nameType");
+const recClassChart = dc.rowChart("#recClass");
+const fallChart = dc.rowChart("#fall");
+// dc.config.defaultColors(d3.schemeCategory10);
 d3.csv("assets/data/meteorite-landings.csv")
   .catch((err) => {
     throw err;
@@ -5,5 +9,35 @@ d3.csv("assets/data/meteorite-landings.csv")
   .then(function (data) {
     const ndx = crossfilter(data);
     const all = ndx.groupAll();
-    console.log(all.value());
+
+    const nameTypeDim = ndx.dimension((d) => d.nametype);
+    const recClassDim = ndx.dimension((d) => d.recclass);
+    const fallDim = ndx.dimension((d) => d.fall);
+
+    const nameTypeGroup = nameTypeDim.group();
+    const recClassGroup = recClassDim.group();
+    const fallGroup = fallDim.group();
+
+    nameTypeChart
+      .dimension(nameTypeDim)
+      .group(nameTypeGroup)
+      .elasticX(true);
+
+    
+    recClassChart
+      // .height(500)
+      .dimension(recClassDim)
+      .group(recClassGroup)
+      .data((group) => group.top(10))
+      .elasticX(true);
+      
+        
+    fallChart
+      .dimension(fallDim)
+      .group(fallGroup)
+      .elasticX(true);
+
+
+
+    dc.renderAll();    
   });

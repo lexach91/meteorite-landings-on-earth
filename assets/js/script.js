531708c9cd9cd6cf783725a1f8ab8b88d1ac071e
lexach91 <konovalov1991@mail.ru>
2021-09-22 16:16:33 +0800

    Add map and plot markers of some meteors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const nameTypeChart = dc.rowChart("#nameType");
-const recClassChart = dc.rowChart("#recClass");
-const fallChart = dc.rowChart("#fall");
+const recClassChart = dc.pieChart("#recClass");
+const fallChart = dc.pieChart("#fall");
 const yearChart = dc.barChart("#year");
 // dc.config.defaultColors(d3.schemeCategory10);
 d3.csv("assets/data/meteorite-landings.csv")
@@ -11,41 +11,72 @@ d3.csv("assets/data/meteorite-landings.csv")
     const ndx = crossfilter(data);
     const all = ndx.groupAll();
 
+    const allDim = ndx.dimension((d) => d);
     const nameTypeDim = ndx.dimension((d) => d.nametype);
     const recClassDim = ndx.dimension((d) => d.recclass);
     const fallDim = ndx.dimension((d) => d.fall);
     const yearDim = ndx.dimension((d) => d.year);
+    const coords = ndx.dimension((d) => [d.reclong, d.reclat]);
+    const mass = ndx.dimension((d) => d.mass);
+
+    coords.filter((d) => d[0] !== "");
+
+    // console.log(yearDim.bottom(10));
 
     const nameTypeGroup = nameTypeDim.group();
     const recClassGroup = recClassDim.group();
     const fallGroup = fallDim.group();
     const yearGroup = yearDim.group();
+    const coordsGroup = coords.group();
+    // console.log(coordsGroup.top(1))
+    nameTypeChart.dimension(nameTypeDim).group(nameTypeGroup).elasticX(true);
 
-    nameTypeChart
-      .dimension(nameTypeDim)
-      .group(nameTypeGroup)
-      .elasticX(true);
-
-    
     recClassChart
       // .height(500)
       .dimension(recClassDim)
       .group(recClassGroup)
-      .data((group) => group.top(10))
-      .elasticX(true);
-      
-        
-    fallChart
-      .dimension(fallDim)
-      .group(fallGroup)
-      .elasticX(true);
+      .data((group) => group.top(10));
+    //   .elasticX(true);
+
+    fallChart.dimension(fallDim).group(fallGroup);
+    //   .elasticX(true);
 
     yearChart
       .dimension(yearDim)
       .group(yearGroup)
       .elasticY(true)
-    //   .elasticX(true)
+      //   .elasticX(true)
       .x(d3.scaleLinear().domain([1900, 2019]));
 
-    dc.renderAll();    
+    var map = L.map("map").setView([39, 34], 2);
+    var meteoriteMarkers = new L.FeatureGroup();
+
+    L.tileLayer(
+      "https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+      {
+        id: "satellite-streets-v11",
+        accessToken:
+          "pk.eyJ1IjoibGV4YWNoOTEiLCJhIjoiY2t0djA5ZDduMDNzaTJwbjRpM2FvcWllNiJ9.lcz7-gBRDJ__hxv1vRjU4g",
+        maxZoom: 16,
+      }
+    ).addTo(map);
+
+    for (let meteor of allDim.top(200)) {
+      // var loc = [d.reclat, d.reclong]
+      var meteorName = meteor.name;
+      var meteorMass = meteor.mass;
+      var marker = L.marker([meteor.reclat, meteor.reclong]);
+      marker.bindPopup("<p>" + meteorName + " Mass: " + meteorMass + "</p>")
+      meteoriteMarkers.addLayer(marker);
+    }
+
+    map.addLayer(meteoriteMarkers);
+    map.fitBounds(meteoriteMarkers.getBounds());
+
+
+
+    dc.renderAll();
   });
+
+
+
